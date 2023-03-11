64baf5ea839364d47ac2fd4ec85c5fa7fa39f113
Chandrachur Mukherjee <chandrachur67@gmail.com>
2023-03-11 11:30:20 +0530

    Added HomepageTeachers.js and modified App.css and index.css files

---
+++ client/src/components/HomepageTeacher.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import react from '../logo.svg'
+import './Homepage.css'
+
+function HomepageTeacher() {
+  return (
+    <div className="homepage">
+      <img className="homepage_logo" src={react} />
+    </div>
+  )
+}
+
+export default HomepageTeacher

cdeb4413f4eed7b29968783d262a033bacf08f1d
Tsaha11 <tuhin727066@gmail.com>
2024-01-19 14:54:17 +0530

    home page

--- client/src/components/InstructorHome.jsx
+++ client/src/components/InstructorHome.jsx
@@ -1,6 +1,23 @@
+import "./HomepageTeacher.css"
 const InstructorHome=()=>{
     return <>
-        <h1>Home Page</h1>
+        <div className="home-page">
+            <div className="header1">
+                <span className="line1"></span>
+                <h5>Videos</h5>
+                <span className="line2"></span>
+            </div>
+            <div className="video-">
+
+            </div>
+        </div>
+        <div className="home-page">
+            <div className="header1">
+                <span className="line1"></span>
+                <h5>Courses</h5>
+                <span className="line2"></span>
+            </div>
+        </div>
     </>
 }
 export default InstructorHome

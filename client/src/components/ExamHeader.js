a26c8660ad7efcbb1bbd00d6b7534adacc5e48f2
Tsaha11 <tuhin727066@gmail.com>
2024-01-19 17:01:39 +0530

    Exam corner fixed

--- client/src/components/ExamHeader.js
+++ client/src/components/ExamHeader.js
@@ -1,6 +1,12 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './ExamCorner.css'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import detail from './examdata.json';
 const ExamHeader = () => {
   const exams=["jee","neet","ctet","nda","upsc"];
@@ -29,83 +35,89 @@ const ExamHeader = () => {
 
   },[key])
   return (
-    <div className={"exam-container"}>
-      <div className={"header"}>
-        <div id="jee" onClick={jeeHandler} className={key===0?'active':''}>
-          <span>JEE</span>
+    <div className='outer'>
+      <div className={"exam-container"}>
+        <div className={"header"}>
+          <div id="jee" onClick={jeeHandler} className={key===0?'active':''}>
+            <span>JEE</span>
+          </div>
+          <div id="neet" onClick={neetHandler} className={key===1?'active':''}>
+            <span>NEET</span>
+          </div>
+          <div onClick={ctetHandler} className={key===2?'active':''}>
+            <span>CTET</span>
+          </div>
+          <div onClick={ndaHandler} className={key===3?'active':''}>
+            <span>NDA</span>
+          </div>
+          <div onClick={upscHandler} className={key===4?'active':''}>
+            <span>UPSC</span>
+          </div>
         </div>
-        <div id="neet" onClick={neetHandler} className={key===1?'active':''}>
-          <span>NEET</span>
-        </div>
-        <div onClick={ctetHandler} className={key===2?'active':''}>
-          <span>CTET</span>
-        </div>
-        <div onClick={ndaHandler} className={key===3?'active':''}>
-          <span>NDA</span>
-        </div>
-        <div onClick={upscHandler} className={key===4?'active':''}>
-          <span>UPSC</span>
-        </div>
-      </div>
-      <div className={"blog-card"}>
-        <input type="radio" name="select" id="tap-1" checked></input>
-        <input type="checkbox" id="imgTap"></input>
-        <div className={"inner-part"}>
-          <label for="imgTap" class="img">
-            <img className={"img-1"} alt="" src={data[key].examImg}></img>
-          </label>
-          <div className={"content content-1"}>
-            <div className={"title"}>{data[key].examName}</div>
-            <h3>Registration Date</h3>
-            <div className={"text"}>{data[key].examReg}</div>
-            <h3>Exam Date</h3>
-            <div className={"text"}>{data[key].examDate}</div>
-            <h3>Role/Position offered</h3>
-            <div className={"text"}>{data[key].examRole}</div>
-            <h3>Official Website Link</h3>
-            <div className={"text"}>Follow this <a href={data[key].examUrl} target="_blank">Link</a></div>
-            <h3>Syllabus</h3>
-            <div className={"text"}>
-              {data[key].examSyllabus}
-              <br></br>
-              <a href={data[key].examSyllabusUrl} target="_blank">
-              <label style={{"fontSize":"1.2rem"}} className="linkSyllabus">To know more </label>
-              </a>
+        <div className={"blog-card"}>
+          <input type="radio" name="select" id="tap-1" checked></input>
+          <input type="checkbox" id="imgTap"></input>
+          <div className={"inner-part"}>
+            <label for="imgTap" class="img">
+              <img className={"img-1"} alt="" src={data[key].examImg}></img>
+            </label>
+            <div className={"content content-1"}>
+              <div className={"title"}>{data[key].examName}</div>
+              <h3>Registration Date</h3>
+              <div className={"text"}>{data[key].examReg}</div>
+              <h3>Exam Date</h3>
+              <div className={"text"}>{data[key].examDate}</div>
+              <h3>Role/Position offered</h3>
+              <div className={"text"}>{data[key].examRole}</div>
+              <h3>Official Website Link</h3>
+              <div className={"text"}>Follow this <a href={data[key].examUrl} target="_blank">Link</a></div>
+              <h3>Syllabus</h3>
+              <div className={"text"}>
+                <br></br>
+                <a href="" target="_blank">
+                <label style={{"fontSize":"1.2rem"}} className="linkSyllabus">To know more </label>
+                </a>
+              </div>
             </div>
           </div>
         </div>
+        <br></br>
+        <div className={"resourceContainer"}>
+        <h2>Best Youtube Channel</h2>
+        <div className={"channel"}>
+          {
+            data[key].examVideoLinks.map((obj)=>(
+              <div>
+                <a href={obj.url} target="_blank">
+                  <img src={obj.img} alt=""></img>
+                </a>
+                <span className={"header"}>{obj.name}</span>
+                <span clasNames={"info"}>Best youtube channel Link above</span>
+              </div>
+            ))
+          }
+        </div>
+        <h2>Best Resouces Channel</h2>
+        <div className={"resources"}>
+        <Card sx={{ maxWidth: 245 }}>
+          <CardMedia
+            sx={{ height:140  }}
+            image="/profile-13.png"
+          />
+          <CardContent className='notes-content'>
+            <Typography variant="h6" component="div" className='notes-header'>
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over 6,000
+              species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </Card>
+        </div>
       </div>
-      <br></br>
-      <div className={"resourceContainer"}>
-      <h2>Best Youtube Channel</h2>
-      <div className={"channel"}>
-        {
-          data[key].examVideoLinks.map((obj)=>(
-            <div>
-              <a href={obj.url} target="_blank">
-                <img src={obj.img} alt=""></img>
-              </a>
-              <span className={"header"}>{obj.name}</span>
-              <span clasNames={"info"}>Best youtube channel Link above</span>
-            </div>
-          ))
-        }
       </div>
-      <h2>Best Resouces Channel</h2>
-      <div className={"resources"}>
-        {
-          data[key].examResources.map((obj)=>(
-            <div className={"resourceCard"}>
-              <a href={obj.url} target="_blank">
-                <img src={obj.img} alt=""></img>
-              </a>
-              <span className={"header"}>{obj.name}</span>
-              <span className={"info"}>Start practicing by solving questions</span>
-            </div>
-          ))
-        }
-      </div>
-    </div>
+
     </div>
   );
 };

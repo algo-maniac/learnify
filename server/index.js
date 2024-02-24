4ae295f6e253f4bc24c22ed0497fd1db2995280d
Chandrachur Mukherjee <chandrachur67@gmail.com>
2024-02-24 13:59:32 +0530

    moved app.listen inside callback of mongodb

--- server/index.js
+++ server/index.js
@@ -36,20 +36,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/uploads", express.static("uploads"));
 
-const url = String(process.env.MONGO_URI);
-const connectionParams = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
 
-mongoose
-  .connect(url, connectionParams)
-  .then(() => {
-    console.log("Connected to the database ");
-  })
-  .catch((err) => {
-    console.error(`Error connecting to the database. n${err}`);
-  });
 
  
 
@@ -103,10 +90,26 @@ app.post("/search", authenticateGeneral, async (req, res) => {
   }
 });
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log("Some error occured");
-    return;
-  }
-  console.log(`Server running on port ${port}`);
-});
+const url = String(process.env.MONGO_URI);
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(url, connectionParams)
+  .then(() => {
+    console.log("Connected to the database ");
+    app.listen(port, (err) => {
+      if (err) {
+        console.log("Some error occured");
+        return;
+      }
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. n${err}`);
+  });
+
+
